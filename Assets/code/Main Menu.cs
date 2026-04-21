40bc8a552d0cf8ae8bb9f64e0398fea3cdd73f52
MaomaoChicken <saranrat4007@gmail.com>
2026-04-21 08:32:32 +0700

    joke
    add scene

---
+++ Assets/code/Main Menu.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void startgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void mainmenu()
+    {
+        
+    }
+    public void exitgame()
+    {
+        
+    }
+}
