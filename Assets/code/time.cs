2a960f9c42181bb58788e68e4814bb9e0d2c5f67
MaomaoChicken <saranrat4007@gmail.com>
2026-04-21 17:43:16 +0700

    add code

--- Assets/code/time.cs
+++ Assets/code/time.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MK : MonoBehaviour
+public class time : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
