c74cfddca53c33cc3aedaaee7ec44cf0ed8f0546
Moshichicken <34920789g@gmail.com>
2026-04-23 20:49:12 +0700

    Book system

--- Assets/code/Main Menu.cs
+++ Assets/code/Main Menu.cs
@@ -9,6 +9,43 @@ public class MainMenu : MonoBehaviour
     public Image sound;
     public Sprite sound_off;
     public Sprite sound_on;
+    public GameObject cha1;
+    public GameObject cha2;
+    public GameObject cha3;
+    public GameObject cha4;
+    public GameObject cha5;
+    public GameObject cha6;
+    public GameObject cha7;
+    public GameObject cha8;
+    public int save_cha = 0;
+   
+    public void hide_character()
+    {
+        cha1.SetActive(false); cha2.SetActive(false);
+        cha3.SetActive(false); cha4.SetActive(false);
+        cha5.SetActive(false); cha6.SetActive(false);
+        cha7.SetActive(false); cha8.SetActive(false);
+       
+    }
+
+    public void save_char(int save_cha)
+    {
+        this.save_cha = save_cha;
+    }
+    public void lode_char()
+    {
+        hide_character();
+
+        if (this.save_cha == 0) cha1.SetActive (true);
+        if(this.save_cha == 1) cha2.SetActive (true);
+        if (this.save_cha == 2) cha3.SetActive (true);
+        if( this.save_cha == 3) cha4.SetActive (true);
+        if ( this.save_cha == 4) cha5.SetActive (true);
+        if (this.save_cha == 5) cha6.SetActive (true);
+        if (this .save_cha == 6) cha7.SetActive (true);
+        if(this .save_cha == 7) cha8.SetActive (true);
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void startgame()
