3376481b00090353cc0dad2464c20c7d6c758df4
MaomaoChicken <34920789g@gmail.com>
2026-04-23 20:07:21 +0700

    Moshi
    เครื่องปั่น

---
+++ Assets/code/button alpha2.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonalpha2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
